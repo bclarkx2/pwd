ae2c00d76c8c258370239a8cc67290ec1c40df02
Brian Clark <bclarkx2@gmail.com>
2026-06-29 07:06:32 -0400

    Ignore dirty submodules

--- pwd.py
+++ pwd.py
@@ -97,19 +97,19 @@ def branch_info(repo_path, vcs_subdir):
     if vcs_subdir == ".git":
         return git_branch_info(repo_path, vcs_subdir)
     else:
-        return "SVN", ""
+        return "SVN", "", "", ""
 
 
 def git_branch_info(repo_path, vcs_subdir):
     cmd_output = subprocess.run(
-        ["git", "status", "--branch", "--porcelain"],
+        ["git", "status", "--branch", "--porcelain", "--ignore-submodules=dirty"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
 
     if cmd_output.returncode != 0:
         err = cmd_output.stderr.decode(encoding="utf-8").strip()
-        return "NO STATUS", "", f"[{err}]"
+        return "NO STATUS", "", f"[{err}]", ""
 
     lines = cmd_output.stdout.decode(encoding="utf-8").splitlines()
 
